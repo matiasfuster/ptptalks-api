cec9e1cf032535fcb6ba67769435adfadaab815f
Matías Fuster <matias.fuster@solcre.com>
2019-10-04 16:33:27 -0300

    feat: add error middleware

--- public/index.php
+++ public/index.php
@@ -4,10 +4,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 header("Access-Control-Allow-Origin: *");
  
-use DI\Container;
-use Slim\Factory\AppFactory;
 use Dotenv\Dotenv;
-use Solcre\ptptalks\Entity\Talk;
+use DI\Container;
+use Slim\Middleware\ErrorMiddleware;
+use Slim\Factory\AppFactory;
 use Solcre\ptptalks\Helper\TalkLoader;
 
 date_default_timezone_set('America/Montevideo');
@@ -23,10 +23,22 @@ $container = new Container();
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
+// Add error middleware
+$responseFactory = $app->getResponseFactory();
+$errorMiddleware = new ErrorMiddleware(
+    $app->getCallableResolver(),
+    $responseFactory,
+    getenv('ENVIRONMENT')=='dev',
+    true,
+    true
+);
+$app->add($errorMiddleware);
+
 $app->get('/talks', function ($request, $response, $args) {
     $talksArr = [];
-    
-    $talks = TalkLoader::load(__DIR__ . '/../data/');
+    $talksPath = __DIR__ . getenv('TALKS_RELATIVE_PATH');
+
+    $talks = TalkLoader::load($talksPath);
     foreach ($talks as $talk) {
         $talksArr[] = $talk->toArray();
     }
