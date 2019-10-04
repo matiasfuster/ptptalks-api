956d9eca0b028f4f49b16e39a07e15a6d5e75047
Matías Fuster <matias.fuster@solcre.com>
2019-10-04 10:25:06 -0300

    feat(Talks): add classes to load talks

---
+++ src/Entity/Talk.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace Solcre\ptptalks\Entity;
+
+class Talk
+{ 
+    protected $filename;
+    protected $speaker;
+    protected $avatar;
+    protected $title;
+    protected $image;
+    protected $slide;
+    protected $tags;
+
+    
+
+    /**
+     * Get the value of filename
+     */ 
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    /**
+     * Set the value of filename
+     *
+     * @return  self
+     */ 
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of speaker
+     */ 
+    public function getSpeaker()
+    {
+        return $this->speaker;
+    }
+
+    /**
+     * Set the value of speaker
+     *
+     * @return  self
+     */ 
+    public function setSpeaker($speaker)
+    {
+        $this->speaker = $speaker;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of avatar
+     */ 
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * Set the value of avatar
+     *
+     * @return  self
+     */ 
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of image
+     */ 
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     *
+     * @return  self
+     */ 
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of slides
+     */ 
+    public function getSlides()
+    {
+        return $this->slides;
+    }
+
+    /**
+     * Set the value of slides
+     *
+     * @return  self
+     */ 
+    public function setSlides($slides)
+    {
+        $this->slides = $slides;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of tags
+     */ 
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    /**
+     * Set the value of tags
+     *
+     * @return  self
+     */ 
+    public function setTags($tags)
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+
+    /**
+     * Return the id of the talk
+     *
+     * @return void
+     */
+    public function getId() 
+    {
+        return (int)explode('.json', basename($this->filename))[0];
+    }
+
+    public function getDate()
+    {
+        return date_create_from_format('Ymd', $this->getId());
+    }
+
+    /**
+     * Constructor
+     *
+     * @param string $filename
+     * @param string $speaker
+     * @param string $title
+     * @param Array $tags
+     * @param string $slides
+     * @param string $image
+     * @param string $avatar
+     */
+    public function __construct(
+        $filename, 
+        $speaker, 
+        $title, 
+        Array $tags, 
+        $slides = "", 
+        $image = "", 
+        $avatar = ""
+    ) {
+        $this->setFilename($filename);
+        $this->setSpeaker($speaker);
+        $this->setTitle($title);
+        $this->setTags($tags);
+        $this->setSlides($slides);
+        $this->setImage($image);
+        $this->setAvatar($avatar);
+    }
+
+    public function toArray() {
+        return [
+            "id" => $this->getId(),
+            "date" => $this->getDate(),
+            "speaker" => $this->getSpeaker(),
+            "title" => $this->getTitle(),
+            "tags" => $this->getTags(),
+            "slides" => $this->getSlides(),
+            "image" => $this->getImage(),
+            "avatar" => $this->getAvatar()
+        ];
+    }
+}
---
+++ src/Helper/TalkLoader.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Solcre\ptptalks\Helper;
+
+use Solcre\ptptalks\Entity\Talk;
+
+class TalkLoader 
+{
+    public static function load($path) {
+        $talks = [];
+        
+        if (is_dir($path)) {
+            $files = glob($path . "[0-9]*.json");
+
+            foreach ($files as $file) {
+                $talksDef = json_decode(file_get_contents($file), true);
+                $talks[] = new Talk(
+                    $file, 
+                    $talksDef['speaker'],
+                    $talksDef['title'],
+                    $talksDef['tags'],
+                    $talksDef['slides'],
+                    $talksDef['image'],
+                    $talksDef['avatar']
+                );
+            }
+        }
+
+        return $talks;
+    }
+}
